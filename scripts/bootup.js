4492a3ea85785011309b3117ff1cc79b030e05cc
ZigZag07 <127082290+zigzag-007@users.noreply.github.com>
2024-07-16 00:03:38 +0530

    Add files via upload

--- scripts/bootup.js
+++ scripts/bootup.js
@@ -2,5 +2,5 @@ document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         document.querySelector('.loader-wrapper').style.display = 'none';
         document.querySelector('.content').style.display = 'block';
-    }, 1300); // Adjust the time (2000ms = 2 seconds) as needed
+    }, 1000); // Adjust the timing as needed
 });
